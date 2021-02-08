bdca910da13f0f1ba707d437450e567ecd11fcbc
mibrahim <mi185051@ncr.com>
2021-02-08 15:23:11 +0000

    Added hasmap to get the proxy mapping

--- app.js
+++ app.js
@@ -5,17 +5,36 @@ const config = require('./config.js');
 
 // module variables
 const app = express()
+let configMap = new Map()
 const port = 3001
+const proxyPort = 9999
 
 // environment variables
 process.env.NODE_ENV = 'pte';
+//console.log(`global.gConfig: ${JSON.stringify(global.gConfig, undefined, global.gConfig.json_indentation)}`);
+
+//Enabling router
+app.use('/', controller)
+
+//App Running port
+app.listen(port, () => {
+  console.log(`Node TCProxy Webservice is listening at http://localhost:${port}`);
+
+  var jsonArray = global.gConfig
+  jsonArray = JSON.parse(JSON.stringify(jsonArray));
+  jsonArray.forEach(function(element) {
+    configMap.set(element.sourceHost, element.targetHost+':'+element.targetPort)
+    //console.log("Source Host ::" + element.sourceHost +' Mapped Target ::'+ element.targetHost+':'+element.targetPort)
+  });
 
 
-var serviceHosts = ["apache.org", "apache.org"];
-var servicePorts = [8091, 8092];
-//Enabling proxy
-//exports.createProxy = proxy.createProxy;
-var newProxy = proxy.createProxy(9999, serviceHosts, servicePorts, {
+  var targetHosts = ["apache.org", "apache.org"];
+  var targetPorts = [8090, 8091];
+  //Enabling proxy
+  //exports.createProxy = proxy.createProxy;
+
+  var newProxy = proxy.createProxy(proxyPort, configMap, {
+
     upstream: function(context, data) {
         log(context.proxySocket, data);
         return data;
@@ -33,13 +52,8 @@ var newProxy = proxy.createProxy(9999, serviceHosts, servicePorts, {
         // use your own strategy to calculate i
         return i;
     }
-});
 
-//Enabling router
-app.use('/', controller)
+  });
 
-
-//App Running port
-app.listen(port, () => {
-  console.log(`Node TCProxy app listening at http://localhost:${port}`);
+  console.log(`Node TCProxy is listening at localhost:${port}`);
 })
